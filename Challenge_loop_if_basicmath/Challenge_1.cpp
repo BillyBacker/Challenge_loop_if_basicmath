ce101194fe648ea10373ca3ed69201f04c186d49
BillyBacker <38096723+BillyBacker@users.noreply.github.com>
2020-08-14 18:22:54 +0700

    Remove Input text, Add comment.

--- Challenge_loop_if_basicmath/Challenge_1.cpp
+++ Challenge_loop_if_basicmath/Challenge_1.cpp
@@ -1,26 +1,26 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<math.h>
 
-void show_assend(double min, double max) {
+void show_assend(double min, double max) { //ใช้แสดงค่าตัวเลขเดินหน้า
 	for (int i = min; i <= max; i++) {
 		printf("%d ", i);
 	}
 	printf("\n");
 }
 
-void show_dessend(double min, double max) {
+void show_dessend(double min, double max) { //ใช้แสดงตัวเลขถอยหลัง
 	for (int i = min; i >= max; i--) {
 		printf("%d ", i);
 	}
 	printf("\n");
 }
 
-double avg(double min, double max) {
+double avg(double min, double max) { //ใช้หาค่าเฉลี่ย
 	return (((max - min + 1) / 2) * (min + max))/((max - min + 1));
 }
 
-void SD(double min, double max) {
+void SD(double min, double max) { //ใช้หา SD
 	double SD;
 	double SD2 = 0;
 	for (int i = 0; i < max - min + 1; i++) {
@@ -31,16 +31,15 @@ void SD(double min, double max) {
 }
 int main() {
 	double min, max;
-	printf("Input : ");
 	scanf("%lf %lf", &min, &max);
-	if (min < max) {
+	if (min < max) { //ถ้าตัวแรกน้อยกว่าตัวหลัง
 		show_assend(min, max);
-		printf("%.1f\n", avg(min, max));
+		printf("Average = %.1f\n", avg(min, max));
 		SD(min, max);
 	}
-	else if (max < min) {
+	else if (max < min) { //ถ้าตัวหลังน้อยกว่าตัวแรก
 		show_dessend(min, max);
-		printf("%.1f\n", avg(max, min));
+		printf("Average = %.1f\n", avg(max, min));
 		SD(max, min);
 	}
 	return 0;
