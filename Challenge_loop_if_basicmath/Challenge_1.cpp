67798724c61017c9033f05dff6e280e7c67d3883
BillyBacker <38096723+BillyBacker@users.noreply.github.com>
2020-08-14 18:16:06 +0700

    Add function.

--- Challenge_loop_if_basicmath/Challenge_1.cpp
+++ Challenge_loop_if_basicmath/Challenge_1.cpp
@@ -1,24 +1,47 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<math.h>
 
-int sum, count;
-
-void show(int min, int max) {
+void show_assend(double min, double max) {
 	for (int i = min; i <= max; i++) {
 		printf("%d ", i);
 	}
 	printf("\n");
 }
 
-void avg() {
+void show_dessend(double min, double max) {
+	for (int i = min; i >= max; i--) {
+		printf("%d ", i);
+	}
+	printf("\n");
+}
 
-	printf("Average = %.1f\n", sum/count);
+double avg(double min, double max) {
+	return (((max - min + 1) / 2) * (min + max))/((max - min + 1));
+}
+
+void SD(double min, double max) {
+	double SD;
+	double SD2 = 0;
+	for (int i = 0; i < max - min + 1; i++) {
+		SD2 += pow((avg(min, max) - (min + i)), 2);
+	}
+	SD = sqrt(SD2 / (max - min));
+	printf("SD = %.2lf", SD);
 }
 int main() {
-	int min, max, count;
+	double min, max;
 	printf("Input : ");
-	scanf("%d %d", &min, &max);
-	show(min, max);
-	avg();
+	scanf("%lf %lf", &min, &max);
+	if (min < max) {
+		show_assend(min, max);
+		printf("%.1f\n", avg(min, max));
+		SD(min, max);
+	}
+	else if (max < min) {
+		show_dessend(min, max);
+		printf("%.1f\n", avg(max, min));
+		SD(max, min);
+	}
 	return 0;
 }
